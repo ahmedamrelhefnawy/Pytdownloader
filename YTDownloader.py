2957524734799682782ac02803a5ca6cb3ab57f0
ahmedelhefnawy <ahmedelhefnawy2003@yahoo.com>
2024-01-24 07:26:22 +0200

    Added formatting_streams function

--- YTDownloader.py
+++ YTDownloader.py
@@ -1,1 +1,7 @@
-# test comment
+from pytube import YouTube
+from libs import choices_manip
+yt = YouTube('https://www.youtube.com/shorts/opRm3x9vHH0')
+
+streams = yt.streams.filter(type="video")
+print(streams)
+print(choices_manip.format_streams(streams))
