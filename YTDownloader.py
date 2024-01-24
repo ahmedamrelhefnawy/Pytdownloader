11704496c275397f1c3a336387d4c0d4e8487f75
ahmedelhefnawy <ahmedelhefnawy2003@yahoo.com>
2024-01-24 08:08:47 +0200

    Added  a random comment

--- YTDownloader.py
+++ YTDownloader.py
@@ -2,6 +2,7 @@ from pytube import YouTube
 from libs import choices_manip
 yt = YouTube('https://www.youtube.com/shorts/opRm3x9vHH0')
 
+# text
 streams = yt.streams.filter(type="video")
 print(streams)
 print(choices_manip.format_streams(streams))
