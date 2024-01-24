6e57e6c245394972cb936b21bd1bdb9260ff80ce
Ahmed_Adel <ahmedadel30320032003@gmail.com>
2024-01-24 14:58:04 +0200

    I made the progress bar and the path of the downloading

--- libs/choices_manip.py
+++ libs/choices_manip.py
@@ -1,6 +1,8 @@
 def format_streams(streams):
     streams_list = []
     for stream in streams:
-        streams_list.append({"res":stream.resolution, "fps":stream.fps, "extension":stream.mime_type[7:], "size":stream.filesize_mb, "itag":stream.itag})
-    
+        streams_list.append(
+            {"res": stream.resolution, "fps": stream.fps, "extension": stream.mime_type[7:], "size": stream.filesize_mb,
+             "itag": stream.itag})
+
     return streams_list
